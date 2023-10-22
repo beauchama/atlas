72cc9caf7622548426dfbc9093920f94fda6dc67
beauchama <alexandre.beauchamp96@gmail.com>
2023-10-22 15:39:06 -0400

    feature: added Flag entity

---
+++ src/Atlas.Domain/Flags/Flag.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Atlas.Domain.Geography;
+
+namespace Atlas.Domain.Flags;
+
+public sealed record Flag
+{
+    public required string Code { get; init; }
+
+    public required Translations Translations { get; init; }
+
+    public required Continent Continent { get; init; }
+
+    public required GeographicCoordinate Coordinate { get; init; }
+
+    public required double Area { get; init; }
+}
