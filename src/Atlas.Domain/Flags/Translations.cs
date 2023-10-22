67dce0e7e4e81193e2e9acf18c6c762bac6deca5
beauchama <alexandre.beauchamp96@gmail.com>
2023-10-22 15:13:39 -0400

    feature: added translations and translation (anemic object)

---
+++ src/Atlas.Domain/Flags/Translations.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Atlas.Domain.Flags;
+
+public sealed record Translations
+{
+    public required Translation French { get; init; }
+
+    public required Translation English { get; init; }
+}
