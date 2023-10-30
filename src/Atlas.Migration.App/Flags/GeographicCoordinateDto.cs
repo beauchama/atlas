1cf9d80ba20b7164fd81d11e2c0f975f0114138b
beauchama <alexandre.beauchamp96@gmail.com>
2023-10-29 21:29:46 -0400

    feature: Added GeographicCoordinate dto

---
+++ src/Atlas.Migration.App/Flags/GeographicCoordinateDto.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Atlas.Migration.App.Flags;
+
+internal sealed record GeographicCoordinateDto(double Latitude, double Longitude);
