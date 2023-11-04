6c490473f107191c7844f57414222fc985694c22
beauchama <alexandre.beauchamp96@gmail.com>
2023-11-03 20:14:40 -0400

    feature: Added Atlas.Web.App

---
+++ src/Atlas.Web.App/Program.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Atlas.Web.App;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+await builder.Build().RunAsync().ConfigureAwait(false);
