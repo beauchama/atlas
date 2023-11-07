402391e7d34132ad329a3d4a180d93a02cf6d6be
beauchama <alexandre.beauchamp96@gmail.com>
2023-11-07 18:40:23 -0500

    feature: Added Icon component base

---
+++ src/Atlas.Web.App/Components/Icons/IconSize.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Atlas.Web.App.Components.Icons;
+
+public enum IconSize
+{
+    Small = 0,
+    Medium = 1,
+    Large = 2,
+    XLarge = 3,
+    XXLarge = 4
+}
